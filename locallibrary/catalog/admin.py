0004d3e59907cb9c8004dc4043a389a34a0b7ca5
luuhieu8720 <lethiluuhieu@gmail.com>
2022-02-22 14:52:24 +0700

    Add part4

---
+++ locallibrary/catalog/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
+
+class BooksInline(admin.TabularInline):
+    model = Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BooksInline]
+
+admin.site.register(Genre)
+admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book using the decorator
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book','imprint','id','status','due_back')
