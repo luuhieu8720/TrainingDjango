d10f6655d3e02125e72668d2cf38ea0c656d17aa
luuhieu8720 <lethiluuhieu@gmail.com>
2022-02-25 15:09:10 +0700

    Add part6

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,12 +1,17 @@
+from django.http import Http404
 from django.shortcuts import render
-
-# Create your views here.
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+from django.shortcuts import get_object_or_404
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class AuthorListView(generic.ListView):
+    model = Author 
 
 def index(request):
-    """View function for home page of site."""
-
-    # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
 
     num_instances = BookInstance.objects.all().count()
@@ -25,5 +30,16 @@ def index(request):
         'num_genres': num_genres
     }
 
-    # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    def book_detail_view(request, primary_key):
+        book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'catalog/book_detail.html', context={'book': book})
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    def author_detail_view(request, primary_key):
+        author = get_object_or_404(Author, pk=primary_key)
+        return render(request, 'catalog/author_detail.html', context={'author': author})
